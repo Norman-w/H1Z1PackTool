56a2903e3adcded64070f14a3faef9f2d9dc91e5
Norman-w <85535885@qq.com>
2022-08-07 20:38:14 +0800

    load dir and build pack

--- Program.cs
+++ Program.cs
@@ -67,4 +67,88 @@ void Repack(string packSrcRootDir, string packDestRootDir)
     }
 }
 
-Repack("/Volumes/BOOTCAMP/h1z1/201612/Resources/Assets", "/Volumes/BOOTCAMP/h1z1/Assets_New/");
+//Repack("/Volumes/BOOTCAMP/h1z1/201612/Resources/Assets", "/Volumes/BOOTCAMP/h1z1/Assets_New/");
+
+void DeleteAllFxoFiles()
+{
+    string packSrcRootDir = "/Volumes/BOOTCAMP/h1z1/201612/Resources/Assets";
+    string packDestRootDir = "/Volumes/BOOTCAMP/h1z1/201612/Resources/Assets_no_fxo";
+    Packer packer = new Packer();
+    var packFilesPathList = packer.ListPacks(packSrcRootDir);
+    if (!Directory.Exists(packDestRootDir))
+    {
+        Directory.CreateDirectory(packDestRootDir);
+    }
+    var pos = 1;
+    foreach (var packFilePath in packFilesPathList)
+    {
+        var packFile = packer.LoadPackFile(packFilePath);
+        List<string> fxoFiles = new List<string>();
+        foreach (var asset in packFile.Assets)
+        {
+            if (asset.Value.Name.ToLower().EndsWith(".fxo"))
+            {
+                fxoFiles.Add(asset.Key);
+            }
+        }
+
+        if (fxoFiles.Count>0)
+        {
+            foreach (var fxoFile in fxoFiles)
+            {
+                packFile.Assets.Remove(fxoFile);
+            }
+            packFile.PackFileFullName = Path.Combine(packDestRootDir, packFile.PackFileName) + ".pack";
+            packer.SavePackFile(packFile);
+        }
+
+        Console.WriteLine("正在重存第{0}个资源包", pos++);
+    }
+}
+
+// DeleteAllFxoFiles();
+
+// Packer packer = new Packer();
+// var assetsPathNames = new List<string>();
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_006");
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_032");
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_071");
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_091");
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_144");
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_203");
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_231");
+// assetsPathNames.Add("/Users/coolking/Desktop/assets/Assets_251");
+// for (int i = 0; i < assetsPathNames.Count; i++)
+// {
+//     var packFile = packer.LoadAssetsIntoPackFile(assetsPathNames[i]);
+//     packer.SavePackFile(packFile);
+//     Console.WriteLine("文件夹{0}已经打包完成", assetsPathNames[i]);
+// }
+
+void LoadDirAndBuildPack()
+{
+    string packSrcRootDir = "/Volumes/BOOTCAMP/h1z1/AssetsOut/all_default";
+    string packDestRootDir = "/Volumes/BOOTCAMP/h1z1/201612/Resources/Assets";
+    Packer packer = new Packer();
+    var packFilesPathList = Directory.GetDirectories(packSrcRootDir);
+    if (!Directory.Exists(packDestRootDir))
+    {
+        Directory.CreateDirectory(packDestRootDir);
+    }
+    var pos = 1;
+    foreach (var packFilePath in packFilesPathList)
+    {
+        var packFile = packer.LoadAssetsIntoPackFile(packFilePath);
+
+        packFile.PackFileFullName = Path.Combine(packDestRootDir, packFile.PackFileName) + ".pack";
+        packer.SavePackFile(packFile);
+        Console.WriteLine("正在重存第{0}个资源包", pos++);
+    }
+}
+
+LoadDirAndBuildPack();
+//
+// Packer packer = new Packer();
+// var packFile = packer.LoadAssetsIntoPackFile("/Volumes/BOOTCAMP/h1z1/AssetsOut/all/Assets_256");
+// packFile.PackFileFullName = "/Volumes/BOOTCAMP/h1z1/201612/Resources/Assets/Assets_256.pack";
+// packer.SavePackFile(packFile);
